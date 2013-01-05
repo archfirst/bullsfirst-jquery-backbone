bbfac9c27f259d11e96fbd76a0e45458f42d6d05
Naresh Bhatia <naresh.archfirst@gmail.com>
2013-01-05 15:09:29 -0500

    Moved css includes from index.html to index.scss.

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,98 @@
  *
  * @author Naresh Bhatia
  */
+require.config({
+    // Initialize the application with the main application file
+    deps: ['main'],
+
+    paths: {
+        // jQuery
+        jquery:                      'vendor/jquery-1.8.3',
+        jqueryui:                    'vendor/jquery-ui-1.8.23.custom.min',
+        jqueryValidationEngine:      'vendor/jquery.validationEngine-2.5.2',
+        jqueryValidationEngineRules: 'vendor/jquery.validationEngine-en-2.5.2',
+        jqueryTreeTable:             'vendor/jquery.treeTable-20121109',
+
+        // Underscore
+        underscore:                  'vendor/underscore-1.4.3',
+
+        // Backbone
+        backbone:                    'vendor/backbone-0.9.9',
+
+        // Templating
+        handlebars:                  'vendor/handlebars-1.0.rc.1',
+
+        // Base64 encoding for authentication headers
+        base64encode:                'vendor/base64_encode',
+        utf8encode:                  'vendor/utf8_encode',
+
+        // Formatting and parsing of dates and numbers,
+        // similar to SimpleDateFormat and NumberFormat APIs
+        jqueryformat:                'vendor/jquery.format-1.2',
+
+        // Date library
+        moment:                      'vendor/moment-1.7.2',
+
+        // jQuery Alerts (http://www.codeproject.com/Articles/295236/jQuery-UI-Alerts-Dialog-using-ThemeRollers)
+        jqueryalerts:                'vendor/jquery.alerts',
+
+        // Charting
+        highcharts:                  'vendor/highcharts-2.3.3'
+    },
+
+    shim: {
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+
+        base64encode: {
+            deps: ['utf8encode'],
+            exports: 'base64_encode'
+        },
+
+        handlebars: {
+            exports: 'Handlebars'
+        },
+
+        highcharts: {
+            exports: 'Highcharts'
+        },
+
+        jqueryalerts: {
+            deps: ['jquery']
+        },
+
+        jqueryformat: {
+            deps: ['jquery']
+        },
+
+        jqueryTreeTable: {
+            deps: ['jquery']
+        },
+
+        jqueryui: {
+            deps: ['jquery']
+        },
+
+        jqueryValidationEngine: {
+            deps: ['jquery']
+        },
+
+        jqueryValidationEngineRules: {
+            deps: ['jqueryValidationEngine']
+        },
+
+        moment: {
+            exports: 'moment'
+        },
+
+        underscore: {
+            exports: '_'
+        }
+    }
+});
+
 require(
     [
         'bullsfirst/app/App'
